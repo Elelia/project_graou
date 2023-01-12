36e0875aead9c0299b98840d21b4b66a0e75b57b
andrews <andrews.agricole@gmail.com>
2023-01-12 15:40:34 +0100

    [Andrews] actionNight loupgarou

--- model/class.loupgarou.inc.php
+++ model/class.loupgarou.inc.php
@@ -1,21 +1,29 @@
 <?php
 
 
-class loupgarou
+class Loupgarou extends Personnage
 {
-    private $id;
 
-    public function __construct($id)
+    public function __construct($id,$carte_id,$name_pers,$life,$status)
     {
-        $this->id=$id;
+        parent::__construct($id,$carte_id,$name_pers,$life,$status,null);
     }
 
-    public function get_id()
-    {
-        return $this->id;
-    }
-    public function set_id($id) {
-        $this->id=$id;
-    }
     
+    /**
+     * un loup garou graille des villageois la nuit
+     * @param $objectPersonnage liste de personnage
+     * @param $idPersonneSelect Id du personnage choisis 
+     */
+    public function ActionNuit($idPersonneSelect, $objectPersonnage)
+    {
+      foreach($objectPersonnage as $personnage)
+      {
+        if($personnage->get_Id() == $idPersonneSelect)
+        {
+          // set life to 0 when  characters get killed 
+          $personnage->set_life(0);
+        }
+      }
+    }  
 }
