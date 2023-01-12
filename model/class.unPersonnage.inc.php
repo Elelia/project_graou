1706111e2d63836cb0aec9c120a5bbc1ee06c890
Elelia <llucas@ensup.eu>
2023-01-12 11:25:50 +0100

    update:lisa

--- model/class.unPersonnage.inc.php
+++ model/class.unPersonnage.inc.php
@@ -9,44 +9,55 @@ class unPersonnage
 		return $lesLignes; 
     }
 
-    public static function randomCardId() {
+    public static function giveRandomCardId() {
+        $req="select * from personnage";
+        $res = Database::get_monPdo()->query($req);
+        $res->setFetchMode(PDO::FETCH_OBJ);
+        $res->execute();
+		$lesPersonnages = $res->fetchAll();
+        //var_dump($lesPersonnages);
         $distribCarte = array(1,1,2,2,2,2,3,3);
-    }
-
-    public static function giveIdCard($objectPersonnage, $user) {
-        var_dump($objectPersonnage);
-        $distribCarte = array(1,1,2,2,2,2,3,3);
-        foreach($objectPersonnage as $indexPers=>$personnage) {
-			shuffle($distribCarte);
+        var_dump($distribCarte);
+        foreach($lesPersonnages as $personnage) {
+            shuffle($distribCarte);
+            var_dump($distribCarte);
 			$nb=0;
 			foreach($distribCarte as $indexCarte=>$carte) {
+                var_dump($carte);
 				if($nb==0) {
-					$personnage->set_carteId($carte);
-					if($personnage->get_namePers() == 'UserTest') {
-						$user->set_carteId($carte);
-					}
+					$req="update personnage set carte_id = '$carte'";
+                    Database::get_monPdo()->exec($req);
 					unset($distribCarte[$indexCarte]);
 					$nb = 1;
 				}
 			}
-			var_dump($personnage->get_carteId());
-		}
+        }
+        $req = "select p.id, p.carte_id, p.name_pers, p.life, p.status, c.name, c.id from personnage p INNER JOIN carte c ON c.id = p.carte_id";
+        $res = Database::get_monPdo()->query($req);
+        $lesLignes = $res->fetchAll();
+        //var_dump($lesLignes);
+        return $lesLignes;
     }
 
-    /**
-     * Retourne l'ensemble des personnages
-     *
-     * @return Personnages[] tableau d'objet livre
-     */
-    public static function findAllPersonnage() : array
-    {
-        $texteReq = "select p.id, p.carte_id, p.name_pers, p.life, p.status, c.name, c.id from personnage p INNER JOIN carte c ON c.id = p.carte_id";
-        $req = MonPdo::getInstance()->prepare($texteReq);
-        $req->setFetchMode(PDO::FETCH_OBJ);
-        $req->execute();
-        $les_resultats = $req->fetchAll();
-        return $les_resultats;
-    }
+    // public static function giveIdCard($objectPersonnage, $user) {
+    //     var_dump($objectPersonnage);
+    //     $distribCarte = array(1,1,2,2,2,2,3,3);
+    //     foreach($objectPersonnage as $indexPers=>$personnage) {
+	// 		shuffle($distribCarte);
+	// 		$nb=0;
+	// 		foreach($distribCarte as $indexCarte=>$carte) {
+	// 			if($nb==0) {
+	// 				$personnage->set_carteId($carte);
+	// 				if($personnage->get_namePers() == 'UserTest') {
+	// 					$user->set_carteId($carte);
+	// 				}
+	// 				unset($distribCarte[$indexCarte]);
+	// 				$nb = 1;
+	// 			}
+	// 		}
+	// 		var_dump($personnage->get_carteId());
+	// 	}
+    // }
 
 }
 
