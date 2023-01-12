13d4545a7d282ca32d4de2a66ca42924cbd76879
alicia7818 <95030087+alicia7818@users.noreply.github.com>
2023-01-12 15:47:56 +0100

    classe voyante Alicia

---
+++ model/class.voyante.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+class Voyante extends Personnage
+{
+    public function __construct($id,$carte_id,$name_pers,$life,$status)
+    {
+        parent::__construct($id,$carte_id,$name_pers,$life,$status);
+    }
+
+    public static function chooseCard($idCarteSelec){
+        $req="select c.name from carte c inner join personnage p on p.carte_id = c.id where p.id = '$idCarteSelec'";
+        $res = Database::get_monPdo()->query($req);
+		$lesLignes = $res->fetchAll();
+		return $lesLignes;
+
+    }
+}
